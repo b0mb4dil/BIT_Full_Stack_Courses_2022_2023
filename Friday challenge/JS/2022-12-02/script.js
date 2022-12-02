57ac67cf229d5fd6c421608158595cdea27cf81b
Enigma <eivydas.gricius@gmail.com>
2022-12-02 19:54:41 +0200

    javascript friday challenge

---
+++ Friday challenge/JS/2022-12-02/script.js
@@ -0,0 +1,82 @@
+function rand(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// Task - 1
+// Įmonė parduoda žvakes po 1 EUR. Perkant daugiau kaip už 1000 EUR taikoma 3 % nuolaida, daugiau kaip už 2000 EUR - 4 % nuolaida. Parašykite programą, kuri skaičiuos žvakių kainą ir atspausdintų atsakymą kiek žvakių ir kokia kaina perkama. Žvakių kiekį generuokite rand() funkcija nuo 5 iki 3000.
+console.log('-----Task - 1-----')
+console.log('------------------')
+
+
+let candleQuantity = rand(5, 3000)
+console.log(`Žvakių kiekis: ${candleQuantity}`)
+
+if (candleQuantity > 1000 && candleQuantity < 2000) {
+    let candlePrice = candleQuantity * 1 * 0.97;
+    console.log(`Žvakių kaina su 3% nuolaida: ${candlePrice}`);
+} else if (candleQuantity > 2000) {
+    let candlePrice = candleQuantity * 1 * 0.96;
+    console.log(`Žvakių kaina su 4% nuolaida: ${candlePrice}`);
+} else {
+    let candlePrice = candleQuantity * 1 ;
+    console.log(`Žvakių kaina be nuolaidos: ${candlePrice}`);
+}
+
+// Task - 2
+// Naudokite funkcija rand(). Sukurkite tris kintamuosius su atsitiktinėmis reikšmėmis nuo 0 iki 100. Suskaičiuokite jų aritmetinį vidurkį. Ir aritmetinį vidurkį atmetus tas reikšmes, kurios yra mažesnės nei 10 arba didesnės nei 90. Abu vidurkius atspausdinkite. Rezultatus apvalinkite iki sveiko skaičiaus.
+console.log('-----Task - 2-----')
+console.log('------------------')
+
+let num1 = rand(0, 100)
+let num2 = rand(0, 100)
+let num3 = rand(0, 100)
+console.log(`Skaičiai: ${num1}, ${num2}, ${num3}`)
+let average = Math.floor((num1 + num2 + num3)/3)
+console.log(`Trijų skaičių vidurkis: ${average}`)
+
+if (num1 < 10 || num1 > 90) {
+    num1 = 0;
+} if (num2 < 10 || num2 > 90) {
+    num2 = 0;
+} if (num3 < 10 || num3 > 90) {
+    num3 = 0;
+}
+ 
+let averageBelowAbove = Math.floor((num1 + num2 + num3)/3)
+
+console.log(`Vidurkis be < 10 ir > 90 skaičių: ${averageBelowAbove}`)
+
+// Task - 3
+// Padarykite skaitmeninį laikrodį, rodantį valandas, minutes ir sekundes. Valandom, minutėm ir sekundėm sugeneruoti panaudokite funkciją rand(). Sugeneruokite skaičių nuo 0 iki 300. Tai papildomos sekundės. Skaičių pridėkite prie jau sugeneruoto laiko. Atspausdinkite laikrodį prieš ir po sekundžių pridėjimo ir pridedamų sekundžių skaičių.
+console.log('-----Task - 3-----')
+console.log('------------------')
+
+let randHour = rand(23, 23)
+let randMinute = rand(50, 59)
+let randSecond = rand(50, 59)
+
+
+
+console.log(`Sugeneruotas laikas: ${randHour}h, ${randMinute}min, ${randSecond}s.`)
+
+let additionalSeconds = rand(0, 300)
+console.log(`Papildomos sekundės: ${additionalSeconds}s`)
+
+function newTime(h, min, s, add) {
+    h = h * 3600;
+    min = min * 60;
+    let newTime = h + min + s + add
+
+    newHour =  Math.floor(newTime / 3600)
+    newMinute = Math.floor((newTime - (newHour * 3600)) / 60)
+    newSecond = (newTime - (newHour * 3600) - (newMinute * 60))
+
+    if (newHour >= 24 && newMinute > 0 && newSecond > 0) {
+        newHour = 0;
+    }
+    console.log(`Naujas laikas: ${newHour}h, ${newMinute}min, ${newSecond}s.`)
+}
+
+newTime(randHour, randMinute, randSecond, additionalSeconds)
---
+++ Papildomos-uzduotys/2022-12-01-javascript/script.js
@@ -0,0 +1,77 @@
+// Task - 1
+console.log('-----Task - 1-----')
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.random() * (max - min + 1) + min;
+}
+
+let rand1 = getRandomInt(0, 4)
+let rand2 = getRandomInt(0, 4)
+if (rand1 >= rand2) {
+    let result = rand2/rand1;
+    console.log(`Atsitiktiniai skaičiai: rand1 ${rand1}, rand2 ${rand2}`);
+    console.log(`Atsakymas: ${result}`);
+    let resultRounded = parseFloat(result.toFixed(2));
+    console.log(`Suapvalintas rezultatas: ${resultRounded}`);
+} else {
+    let result = rand1/rand2;
+    console.log(`Atsitiktiniai skaičiai: rand1 ${rand1}, rand2 ${rand2}`);
+    console.log(`Atsakymas: ${result}`);
+    let resultRounded = result.toFixed(2);
+    console.log(`Suapvalintas rezultatas: ${resultRounded}`);
+}
+
+
+// Task - 2
+console.log('-----Task - 2-----')
+
+function getRandomIntFloor(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+let x = getRandomIntFloor(0, 24)
+let y = getRandomIntFloor(0, 24)
+let z = getRandomIntFloor(0, 24)
+console.log(`Kintamieji: x=${x}, y=${y}, z=${z}`)
+
+if (Math.min(x, y, z) < x && Math.max(x, y, z) > x) {
+    console.log('Vidurinysis kintamasis x.');
+} else if (Math.min(x, y, z) < y && Math.max(x, y, z) > y) {
+    console.log('Vidurinysis kintamasis y.');
+} else if (Math.min(x, y, z) < z && Math.max(x, y, z) > z) {
+    console.log('Vidurinysis kintamasis z.');
+} else if (x === z && x != y) {
+    console.log('x ir z lygūs')
+} else if (x === y && x != z) {
+    console.log('x ir y lygūs')
+} else if (y === z && y != x) {
+    console.log('y ir z lygūs')
+} else if (y === z && y === x && x === z ) {
+    console.log('x, y ir z lygūs')
+}
+
+
+// Task - 3
+console.log('-----Task - 3-----')
+
+let starName = 'Bruce'
+let starSurname = 'Lee'
+
+let newStar = starName.substring(starName.length - 3) + starSurname.substring(starSurname.length - 3);
+console.log(newStar);
+
+// Task - 4
+console.log('-----Task - 4-----')
+
+let abc = 'abcdefghijklmnopqrstuvwxyz'
+let letterOneIndex = getRandomIntFloor(0, 26)
+let letterTwoIndex = getRandomIntFloor(0, 26)
+let letterThreeIndex = getRandomIntFloor(0, 26)
+
+let resultLetters = abc[letterOneIndex] + abc[letterTwoIndex] + abc[letterThreeIndex];
+console.log(`Raidžių index'ai: pirmos ${letterOneIndex}, antros ${letterTwoIndex}, trečios ${letterThreeIndex}`)
+console.log(`Gautos trys atsitiktinės raidės: ${resultLetters}`);
