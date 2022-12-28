030f0f15431bbc729a87b166da3c6f3a3cd1660a
Enigma <eivydas.gricius@gmail.com>
2022-12-28 19:32:40 +0200

    2022-12-28

---
+++ 2022-12-27 DOM/script.js
@@ -0,0 +1,19 @@
+let btn = document.querySelector('#new-quote');
+let quote = document.querySelector('.quote');
+let person = document.querySelector('.person')
+
+
+btn.addEventListener('click', function() {
+    fetch("https://type.fit/api/quotes")
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+        console.log(data[0])
+        let random = Math.floor(Math.random() * data.length);
+        quote.innerText = data[random].text;
+        person.innerText = data[random].author;
+    });
+
+    
+})
