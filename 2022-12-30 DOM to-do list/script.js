cf0f78c13cb85e789c7ffd972a13ea4f58b0b8fb
Enigma <eivydas.gricius@gmail.com>
2022-12-30 17:40:21 +0200

    2022-12-30 grocery list app

---
+++ 2022-12-30 DOM to-do list/script.js
@@ -0,0 +1,44 @@
+let addTask = document.getElementById('add-task');
+let taskContainer = document.getElementById('task-container');
+let inputTask = document.getElementById('input-task');
+
+addTask.addEventListener('click', function() {
+    let task = document.createElement('div')
+    task.classList.add('task');
+
+    let li = document.createElement('li');
+    li.innerText = `${inputTask.value}`;
+    task.appendChild(li);
+
+    let checkButton = document.createElement("button");
+    checkButton.innerHTML = '<i class="fa-solid fa-check"></i>';
+    checkButton.classList.add('checkTask');
+    task.appendChild(checkButton);
+
+    let deleteButton = document.createElement("button");
+    deleteButton.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+    deleteButton.classList.add('deleteTask');
+    task.appendChild(deleteButton);
+
+    if (inputTask.value === "") {
+        alert('Prašome įvesti prekę')
+    } else {
+        taskContainer.appendChild(task)
+    }
+
+    inputTask.value = "";
+
+    checkButton.addEventListener('click', function() {
+        checkButton.parentElement.style.textDecoration = "line-through";
+    });
+
+    deleteButton.addEventListener('click', function (e) {
+		let target = e.target;
+		if (target.matches('button')) {
+			target.parentElement.remove();
+		}
+		if (target.matches('i')) {
+			target.parentElement.parentElement.remove();
+		}
+	});
+})
