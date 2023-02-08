d9b20ae06c37af41b9d98c6ed7db7375297c4648
Enigma <eivydas.gricius@gmail.com>
2023-02-08 22:40:53 +0200

    2023-02-08-twitter

--- 2023-PHP/2023-02-08-twitter-app/views/account.php
+++ 2023-PHP/2023-02-08-twitter-app/views/account.php
@@ -94,90 +94,115 @@
     });
     
 ?>
-<h1 class="d-flex justify-content-between align-items-center mt-2">
-    Sveiki, <?= $user->name; ?> 
-    <div>
-        <a href="?page=account&action=history" class="btn btn-success">Pavedimų istorija</a>
-        <a href="?page=account&action=new-transfer" class="btn btn-success">Naujas pavedimas</a>
-        <a href="./" class="btn btn-warning">Atsijungti</a>
-    </div>
-</h1>
+<div class="row">
+    <div class="col-3 text-center">
+        
 
-<table class="table mt-3">
-    <thead>
-    <tr>
-        <th>#ID</th>
-        <th>Vardas</th>
-        <th>Pavardė</th>
-        <th>Sąskaitos numeris</th>
-        <th>Sąskaitos likutis</th>
-    </tr>
-    </thead>
-    <tbody>
-        <td><?= $user->id; ?></td>
-        <td><?= $user->name; ?></td>
-        <td><?= $user->last_name; ?></td>
-        <td><?= $user->iban; ?></td>
-        <td><?= $user->balance; ?>€</td>
-    </tbody>
-</table>
-
-<?php if(isset($_GET['message'])) : ?>
-    <div class="alert alert-<?= $_GET['status'] ?>">
-        <?= $_GET['message'] ?>
-    </div>
-<?php endif; ?>
-
-<?php if($action === 'new-transfer') : ?>
-    <form method="POST" class="mt-5" style="max-width: 500px">
-        <div class="mb-3">
-            <label class="fs-5 mb-1">Pavedimo gavėjas</label>
-            <select name="recipient" class="form-control">
-                <?php foreach($recipients as $account) : ?>
-                    <option value="<?= $account->id ?>"><?= $account->name . ' ' . $account->last_name ?></option>
-
-                <?php endforeach; ?>
-            </select>
+        <h1 class="justify-content-between align-items-center mt-2">
+        <i class="bi bi-twitter fs-1 text-primary me-2"></i>Sveiki, <?= $user->name; ?> 
+        <div>
+            <a href="?page=account&action=history" class="btn btn-primary rounded-pill fw-semibold ms-1 me-1">Home</a>
+            <a href="?page=account&action=new-transfer" class="btn btn-primary rounded-pill fw-semibold">New Message</a>
+            <a href="./" class="btn btn-outline-primary rounded-pill fw-semibold">Log Out</a>
         </div>
-        <div class="mb-3">
-            <label class="fs-5 mb-1">Pavedimo suma</label>
-            <input type="number" step="0.01" name="amount" class="form-control" />
+        </h1>
+        <div class="d-grid">
+            <div class="fs-4 mb-4 btn btn-light rounded-pill">Home</div>
+            <div class="fs-4 mb-4 btn btn-light rounded-pill">Explore</div>
+            <div class="fs-4 mb-4 btn btn-light rounded-pill">Notifications</div>
+            <div class="fs-4 mb-4 btn btn-light rounded-pill">Messages</div>
+            <div class="fs-4 mb-4 btn btn-light rounded-pill">Bookmarks</div>
+            <div class="fs-4 mb-4 btn btn-light rounded-pill">Twitter Blue</div>
+            <div class="fs-4 mb-4 btn btn-light rounded-pill">Profile</div>
+            <div class="fs-4 mb-4 btn btn-light rounded-pill">More</div>
         </div>
-        <button class="btn btn-primary">Siųsti</button>
-    </form>
-<?php endif; ?>
+        
+    
+    </div>
+    <div class="col-6">
+        <?php if(isset($_GET['message'])) : ?>
+            <div class="alert alert-<?= $_GET['status'] ?>">
+                <?= $_GET['message'] ?>
+            </div>
+        <?php endif; ?>
+        
+            <div class="d-grid mt-5 bg-body-tertiary p-4 rounded-5 text-left">
+                <div>
+                    <h3>Home</h3>
+                </div>
+                    <?php foreach ($user_data as $value) {
+                        $value->history;    
+                    }
+                    ?>
+                <div>
+                    <?php for ($i = 0; $i < count($user->history); $i++) : ?>
+                        <div class="d-grid ">
+                            <div class="fs-6 mb-3 border rounded-5 border-dark-subtle">
+                                <div class="p-3 ms-1 me-1 pb-1 fw-semibold">
+                                    <div class="d-flex justify-content-between align-items-center">
+                                        <span class="fs-5"><?= $value->history[$i]->from; ?></span>
+                                        <span class="text-body-tertiary"><?= $value->history[$i]->date; ?></span>
+                                    </div>
+                                </div>
+                                <div class="m-3 mt-0 mb-0 text-center">
+                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                                </div>
+                                <div class="p-3 fs-6 text-start fw-semibold text-body-secondary">
+                                </div>
+                                <div class="d-flex justify-content-between ms-5 me-5 mb-2">
+                                    <i class="bi bi-heart-fill text-danger"></i>
+                                    <i class="bi bi-share-fill text-primary"></i>
+                                    <i class="bi bi-chat"></i>
+                                    <i class="bi bi-diagram-3-fill text-success "></i>
+                                </div>
+                            </div>
+                        </div>
+                    <?php endfor ?>
+                </div>
 
-<?php if($action === 'history') : ?>
-    <table class="table mt-5" style="max-width: 900px">
-    <thead>
-        <tr>
-            <th>Data</th>
-            <th>Siuntėjas</th>
-            <th>Gavėjas</th>
-            <th>Suma</th>
-        </tr>
-    </thead>
-    <tbody>
-        <!-- <?php foreach ($user_data as $value) : ?>
-            <tr>
-                <td><?= $value->history; ?></td>
-            </tr>
-        <?php endforeach; ?> -->
-        <?php for ($i = 0; $i < count($user->history); $i++) : ?>
-            <tr class=<?php echo ($value->history[$i]->received ? "table-success" : "table-danger") ?>>
-                <td><?= $value->history[$i]->date; ?></td>
-                <td><?= $value->history[$i]->from; ?></td>
-                <td><?= $value->history[$i]->to; ?></td>
+            </div>   
+    </div>
+    <div class="col-3">
+        <form class="d-flex mt-5 bg-body-tertiary p-4 rounded-5" role="search">
+            <button class="btn" type="submit"><i class="bi bi-search"></i></button>
+            <input class="form-control me-2 bg-transparent rounded-pill" type="search" placeholder="Search" aria-label="Search">
+        </form>
+        <?php if($action === 'new-transfer') : ?>
+            <form method="POST" class="mt-5 bg-body-tertiary p-4 rounded-5" style="max-width: 500px">
+                <div class="mb-3">
+                    <label class="fs-5 mb-1">Nauja žinutė</label>
+                    <select name="recipient" class="form-control">
+                        <?php foreach($recipients as $account) : ?>
+                            <option value="<?= $account->id ?>"><?= $account->name . ' ' . $account->last_name ?></option>
 
-                <!-- <?php if ($value->history[$i]->received) : ?>
-                <td class="table-success"><?= $value->history[$i]->amount; ?></td>
-                <?php else : ?>
-                    <td class="table-danger"><?= $value->history[$i]->amount; ?></td>
-                <?php endif ?> -->
+                        <?php endforeach; ?>
+                    </select>
+                </div>
+                <div class="mb-3">
+                    <label class="fs-5 mb-1">Tekstas</label>
+                    <input type="number" step="0.01" name="amount" class="form-control" />
+                </div>
+                <button class="btn btn-primary">Siųsti</button>
+            </form>
+        <?php endif; ?>
+        <div class="d-grid mt-5 bg-body-tertiary p-4 rounded-5 text-left">
+            <h4 class=>Trends for you</h4>
+            <div class="fs-4 mb-2 btn btn-light rounded-pill text-start">Home</div>
+            <div class="fs-4 mb-2 btn btn-light rounded-pill text-start">Explore</div>
+            <div class="fs-4 mb-2 btn btn-light rounded-pill text-start">Notifications</div>
+            <div class="fs-4 mb-2 btn btn-light rounded-pill text-start">Messages</div>
+            <div class="fs-4 mb-2 btn btn-light rounded-pill text-start">Bookmarks</div>
+            <div class="fs-4 mb-2 btn btn-light rounded-pill text-start">Twitter Blue</div>
+            <div class="fs-4 mb-2 btn btn-light rounded-pill text-start">Profile</div>
+            <div class="fs-4 mb-2 btn btn-light rounded-pill text-start">More</div>
+        </div> 
+    </div>
+</div>
 
-                <td><?= $value->history[$i]->amount; ?></td>
-            </tr>
-        <?php endfor ?>
-    </tbody>
-</table>
-<?php endif; ?>
+
+
+
+
+
+
+
